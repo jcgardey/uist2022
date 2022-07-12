90c9cc59733b5ebb561b361a70e55474412093ae
juancruzgardey <juancruzgardey@hotmail.com>
2022-07-12 13:05:56 -0300

    add "registro" page

--- src/Pages/Pages.js
+++ src/Pages/Pages.js
@@ -6,6 +6,7 @@ import { Pasaporte } from './Pasaporte';
 import { Success } from './Success';
 import { Checkout } from './Checkout/Checkout';
 import { Prestamo } from './Prestamo/Prestamo';
+import { RegistroUsuario } from './RegistroUsuario';
 
 export const Pages = () => {
   const [title, setTitle] = useState('Default Title');
@@ -24,6 +25,10 @@ export const Pages = () => {
       <Route path="/pasaporte" element={<Pasaporte onSubmit={onSubmit} />} />
       <Route path="/checkout" element={<Checkout onSubmit={onSubmit} />} />
       <Route path="/prestamo" element={<Prestamo onSubmit={onSubmit} />} />
+      <Route
+        path="/registrarme"
+        element={<RegistroUsuario onSubmit={onSubmit} />}
+      />
       <Route path="/success" element={<Success />} />
     </Routes>
   );
